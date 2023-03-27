0320c1c9ab6e6b3a67743a1fdfb23afff11993aa
ANTAR NANDI <antarnandy.2@gmail.com>
2023-03-27 21:22:51 +0600

    gggg

--- resources/views/frontend/pages/checkout.blade.php
+++ resources/views/frontend/pages/checkout.blade.php
@@ -128,14 +128,14 @@
                             <h2>CART TOTALS</h2>
                             <div class="content">
                                 <ul>
-                                    <li class="order_subtotal" data-price="{{Helper::totalCartPrice()}}">Cart Subtotal<span>${{number_format(Helper::totalCartPrice(),2)}}</span></li>
+                                    <li class="order_subtotal" data-price="{{Helper::totalCartPrice()}}">Cart Subtotal<span>TK {{number_format(Helper::totalCartPrice(),2)}}</span></li>
                                     <li class="shipping">
                                         Shipping Cost
                                         @if(count(Helper::shipping())>0 && Helper::cartCount()>0)
                                         <select name="shipping" class="nice-select">
                                             <option value="">Select your address</option>
                                             @foreach(Helper::shipping() as $shipping)
-                                            <option value="{{$shipping->id}}" class="shippingOption" data-price="{{$shipping->price}}">{{$shipping->type}}: ${{$shipping->price}}</option>
+                                            <option value="{{$shipping->id}}" class="shippingOption" data-price="{{$shipping->price}}">{{$shipping->type}}: TK {{$shipping->price}}</option>
                                             @endforeach
                                         </select>
                                         @else
@@ -144,7 +144,7 @@
                                     </li>
 
                                     @if(session('coupon'))
-                                    <li class="coupon_price" data-price="{{session('coupon')['value']}}">You Save<span>${{number_format(session('coupon')['value'],2)}}</span></li>
+                                    <li class="coupon_price" data-price="{{session('coupon')['value']}}">You Save<span>TK {{number_format(session('coupon')['value'],2)}}</span></li>
                                     @endif
                                     @php
                                     $total_amount=Helper::totalCartPrice();
@@ -153,9 +153,9 @@
                                     }
                                     @endphp
                                     @if(session('coupon'))
-                                    <li class="last" id="order_total_price">Total<span>${{number_format($total_amount,2)}}</span></li>
+                                    <li class="last" id="order_total_price">Total<span>TK {{number_format($total_amount,2)}}</span></li>
                                     @else
-                                    <li class="last" id="order_total_price">Total<span>${{number_format($total_amount,2)}}</span></li>
+                                    <li class="last" id="order_total_price">Total<span>TK {{number_format($total_amount,2)}}</span></li>
                                     @endif
                                 </ul>
                             </div>
--- resources/views/frontend/pages/product_detail.blade.php
+++ resources/views/frontend/pages/product_detail.blade.php
@@ -83,7 +83,7 @@
 								@php
 								$after_discount=($product_detail->price-(($product_detail->price*$product_detail->discount)/100));
 								@endphp
-								<p class="price"><span class="discount">${{number_format($after_discount,2)}}</span><s>${{number_format($product_detail->price,2)}}</s> </p>
+								<p class="price"><span class="discount">TK {{number_format($after_discount,2)}}</span><s>TK {{number_format($product_detail->price,2)}}</s> </p>
 								<p class="description">{!!($product_detail->summary)!!}</p>
 							</div>
 							<!--/ End Description -->
@@ -349,8 +349,8 @@
 								@php
 								$after_discount=($data->price-(($data->discount*$data->price)/100));
 								@endphp
-								<span class="old">${{number_format($data->price,2)}}</span>
-								<span>${{number_format($after_discount,2)}}</span>
+								<span class="old">TK {number_format($data->price,2)}}</span>
+								<span>TK {{number_format($after_discount,2)}}</span>
 							</div>
 
 						</div>
